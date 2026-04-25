c18f1413bf6ed4d6abca66e4c4c9210c87040878
RevIneX <revinex@users.noreply.github.com>
2026-04-25 20:25:44 +0700

    feat: add app configuration

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/spam-detect")
+    MODEL_NAME = os.getenv("MODEL_NAME", "mrm8488/bert-tiny-finetuned-sms-spam-detection")
+    API_KEY = os.getenv("API_KEY", "")
+    MAX_TEXT_LENGTH = 500
+
+config = Config()
